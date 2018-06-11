6dfe792d0d89f9cef61bdefc4702bacf6683fc83
Abdullah Al Rifat <abdullahalrifat95@gmail.com>
2018-06-11 20:19:01 +0600

    add row in order page fixed

--- php_action/fetchProductData.php
+++ php_action/fetchProductData.php
@@ -4,9 +4,22 @@ require_once 'core.php';
 
 $sql = "SELECT product_id, product_name FROM product WHERE status = 1 AND active = 1";
 $result = $connect->query($sql);
+$output = array('data' => array());
+//$data = $result->fetch_all();
+if($result->num_rows > 0) {
 
-$data = $result->fetch_all();
+    // $row = $result->fetch_array();
+    $activeCategories = "";
 
+    while($row = $result->fetch_array()) {
+        $output['data'][] = array(
+            'val1'=>$row[0],
+            'val2'=>$row[1]
+        );
+
+    } // /while
+
+}// if num_rows
 $connect->close();
 
-echo json_encode($data);
+echo json_encode($output);
